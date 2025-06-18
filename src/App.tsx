686d4c5bd7e5f5d7ee1b17f4d280991042965218
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2025-06-18 14:53:17 +0300

    edit app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Layout from './pages/Layout';
-import GetPeopleByAxios from './pages/GetPeopleByAxios';
-import GetPeoplByReactQuery from './pages/GetPeoplByReactQuery';
-import GetPeopleByReduxThunk from './pages/GetPeopleByReduxThunk';
-import ErrorPage from './pages/ErrorPage';
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
+import GetPeopleByAxios from "./pages/GetPeopleByAxios";
+import GetPeoplByReactQuery from "./pages/GetPeoplByReactQuery";
+import GetPeopleByReduxThunk from "./pages/GetPeopleByReduxThunk";
+import ErrorPage from "./pages/ErrorPage";
 
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <GetPeopleByAxios /> },
+        { path: "people-react-query", element: <GetPeoplByReactQuery /> },
+        { path: "/people-redux-thunk", element: <GetPeopleByReduxThunk /> },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index:true, element: <GetPeopleByAxios /> },
-      { path:'people-react-query', element: <GetPeoplByReactQuery /> },  
-      { path:'/people-redux-thunk', element: <GetPeopleByReduxThunk /> },  
-    ],
-  },
-]);
+    basename: "/API-People-Task",
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
